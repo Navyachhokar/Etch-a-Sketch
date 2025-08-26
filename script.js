14901fee46e73a21053d9420f2f79afeb29ca336
Navya <chhokarnavya@gmail.com>
2025-08-26 08:44:18 +0000

    feat: add button to resize grid based on input feat: add random RGB coloring for hover effect

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 const container = document.querySelector('.container');
-function grid() {
-    for(let i = 0; i< 16*16; i++){
+const button = document.querySelector('button');
+button.textContent = "Change Grid Size";
+
+function makeGrid(size) {
+    container.innerHTML = '';
+    
+    for(let i = 0; i<size * size; i++){
+    
         const div = document.createElement('div');
         div.classList.add('box');
+        div.style.flex = `0 0 calc(100%/${size})`;    
+        div.addEventListener('mouseover', () => {
+            const r = Math.floor(Math.random()*255);
+            const g = Math.floor(Math.random()*255);
+            const b = Math.floor(Math.random()*255);
+            div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            
+        });
         container.appendChild(div);
     }
+   
 }
-grid();
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        const random = Math.random();
-        box.style.backgroundColor = `rgb(${random * 255}, ${random * 255}, ${random * 255})`
-        
-    })
-})
+makeGrid(16);
+
+button.addEventListener('click', () => {
+    
+    let size = parseInt(prompt('Enter the number of aquares per side (max 100):',));
+    if(size > 100) size = 100;
+    if( size && size > 0) {
+        makeGrid(size);
+    }
+});
