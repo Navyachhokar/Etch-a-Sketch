16f107a5ab1322de5081a44428fcb955b8242ea1
Navya <chhokarnavya@gmail.com>
2025-08-26 06:54:25 +0000

    feat: add hover effect to grid squares for sketching

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ function grid() {
     }
 }
 grid();
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        const random = Math.random();
+        box.style.backgroundColor = `rgb(${random * 255}, ${random * 255}, ${random * 255})`
+        
+    })
+})
