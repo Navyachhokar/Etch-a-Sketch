1e36eb2b422dec48cafd6dfc6a4115d64a3c66ac
Navya <chhokarnavya@gmail.com>
2025-08-26 12:16:49 +0000

    feat: Add progressive darkening effect with random colors on hover

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
-button.textContent = "Change Grid Size";
 
 function makeGrid(size) {
     container.innerHTML = '';
@@ -9,12 +8,22 @@ function makeGrid(size) {
     
         const div = document.createElement('div');
         div.classList.add('box');
-        div.style.flex = `0 0 calc(100%/${size})`;    
-        div.addEventListener('mouseover', () => {
-            const r = Math.floor(Math.random()*255);
-            const g = Math.floor(Math.random()*255);
-            const b = Math.floor(Math.random()*255);
-            div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        div.style.flex = `0 0 calc(100%/${size})`; 
+
+        const r = Math.floor(Math.random()*256);
+        const g = Math.floor(Math.random()*256);
+        const b = Math.floor(Math.random()*256);
+        div.dataset.opacity = 0;
+          
+        div.addEventListener('mouseenter', () => {
+            let currentOpacity = parseFloat(div.dataset.opacity);
+            if(currentOpacity < 1){
+                currentOpacity += 0.1 ;
+                if(currentOpacity > 1) currentOpacity = 1;
+                div.dataset.opacity = currentOpacity;
+            }
+            
+            div.style.backgroundColor = `rgba(${r},${g},${b},${currentOpacity})`;
             
         });
         container.appendChild(div);
@@ -25,7 +34,7 @@ makeGrid(16);
 
 button.addEventListener('click', () => {
     
-    let size = parseInt(prompt('Enter the number of aquares per side (max 100):',));
+    let size = parseInt(prompt('Enter the number of squares per side (max 100):',));
     if(size > 100) size = 100;
     if( size && size > 0) {
         makeGrid(size);
